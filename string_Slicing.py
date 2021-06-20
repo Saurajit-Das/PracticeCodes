9c03ba719d45e90b4c06911701aaa4c312ce62f3
saurajit <saurajitdas1@gmail.com>
2021-06-20 15:17:49 +0530

    String Slicing initial commit

---
+++ string_Slicing.py
@@ -0,0 +1,4 @@
+name="My name is Khan"
+print(name[0:3]) #0th index is "M" and it will print upto 3rd index(excluding it) which is "n".
+print(name[::-1]) #It will reverse the sentence
+print(name[0::1])
